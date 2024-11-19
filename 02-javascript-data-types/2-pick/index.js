7476fd203055709de504578a869e14d7418b1cf4
Alexander <pavlutkin58@gmail.com>
2024-11-19 23:31:56 +0300

    функция pick для получения нового объекта с указанными свойствами исходного объекта

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    let resultObj = {}
+    if (fields.every(item => item in obj)) {
+        fields.forEach(item => resultObj[item] = '')
+        const filteredObj = Object.fromEntries(Object.entries(obj).filter(item => fields.includes(item[0])))
+        resultObj = Object.assign(resultObj, filteredObj) 
+    }
 
+    return resultObj
 };
